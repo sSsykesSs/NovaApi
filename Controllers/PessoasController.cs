c0c13d234843700815d4bc43ffa03ab667646e82
sSsykesSs <rayan.vitox9@hotmail.com>
2024-10-25 10:23:49 -0300

    Subindo Api

---
+++ Controllers/PessoasController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using NovaApi.Models;
+
+namespace NovaApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PessoasController : ControllerBase
+    {
+        // Rota GET para obter a lista de pessoas do arquivo JSON
+        [HttpGet]
+        public async Task<IActionResult> GetPessoas()
+        {
+            var jsonFilePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "pessoas.json");
+
+            if (!System.IO.File.Exists(jsonFilePath))
+            {
+                return NotFound("Arquivo pessoas.json não encontrado.");
+            }
+
+            var jsonData = await System.IO.File.ReadAllTextAsync(jsonFilePath);
+
+            var pessoas = JsonSerializer.Deserialize<List<Pessoa>>(jsonData);
+
+            return Ok(pessoas);
+        }
+    }
+}
---
+++ Models/Pessoa.cs
@@ -0,0 +1,10 @@
+﻿namespace NovaApi.Models
+{
+    public class Pessoa
+    {
+        public string nome { get; set; }  
+        public string data_nasc { get; set; }
+        public string email { get; set; }
+        public int idade { get; set; }
+    }
+}
